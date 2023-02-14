4ee871b1785be4236de7c3fc633f5f0d4dfc7ee3
weez rodriguez <w0812903@myscc.ca>
2023-02-13 23:48:23 -0500

    added search form
    form works with colors

--- L_Rodriguez_pokemoncards/src/app/app.module.ts
+++ L_Rodriguez_pokemoncards/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
@@ -16,7 +16,8 @@ import { ContentFilterPipe } from './content-filter.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
